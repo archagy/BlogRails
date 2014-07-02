64849466187f7f0d34d82911c7163c1d5f727e02
elopez91 <jesteban.rdz@gmail.com>
2014-07-01 20:07:33 -0500

    Se agrgo un comentario en la linea 8

--- db/migrate/20140617033532_create_users.rb
+++ db/migrate/20140617033532_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :email
       t.string :password
-
+#probando la subida
       t.timestamps
     end
   end
